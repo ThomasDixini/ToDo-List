67848763812ac85aec429efe71b67d326ab84bc5
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2022-12-31 17:45:37 -0300

    Renderizando tasks apenas se tiver algo no Array

--- src/components/TasksContainer.tsx
+++ src/components/TasksContainer.tsx
@@ -2,10 +2,29 @@ import styles from './tasksContainer.module.css';
 import listLogo from '../assets/listLogo.svg'
 
 export function TasksContainer() {
+
+    interface Task {
+        id: number;
+        content: string;
+    }
+
+    const tasks: Task[] = [
+        {
+            id: 1,
+            content: 'Limpar e organizar o quarto'
+        },
+        {
+            id: 2,
+            content: 'Limpar e organizar o quarto'
+        },
+    ];
+
+    const taskListIsEmpty = tasks.length == 0;
+
     return(
         <div className={styles.tasksBox}>
             <header>
-                <div>
+                <div >
                     <strong> Tarefas Criads </strong>
                     <span> 0 </span>
                 </div>
@@ -15,11 +34,21 @@ export function TasksContainer() {
                 </div>
             </header>
             <div className={styles.content}>
-                <img src={listLogo} alt="Logo de Lista" />
-                <p> 
-                    <strong>Você ainda não tem tarefas cadastradas</strong> 
-                    <p>Crie tarefas e organize seus itens a fazer</p>
-                </p>
+                {
+                    taskListIsEmpty ? (
+                        <>
+                            <img src={listLogo} alt="Logo de Lista" />
+                            <p> 
+                                <strong>Você ainda não tem tarefas cadastradas</strong> 
+                                <p>Crie tarefas e organize seus itens a fazer</p>
+                            </p>
+                        </>
+                    ) : (
+                        tasks.map((task) => {
+                            return <p> {task.content} </p>
+                        })
+                    )
+                }
             </div>
         </div>
     );
